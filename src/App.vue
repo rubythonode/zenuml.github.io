f9476a330cd4c69aee708f198f7af97f1c422849
PengXiao <eagle.xiao@gmail.com>
2017-09-20 22:34:46 +1000

    Removed unused layouts and buttons.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <v-app light>
+    <!--By default clipped is false, so we need this attribute and set it as true-->
     <v-navigation-drawer
       persistent
       :mini-variant="miniVariant"
-      :clipped="clipped"
+      clipped="true"
       v-model="drawer"
       enable-resize-watcher
     >
@@ -23,33 +24,14 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar fixed>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn 
+      <v-btn
         icon
         @click.stop="miniVariant = !miniVariant"
       >
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
+
       <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <main>
       <v-container fluid>
@@ -60,21 +42,7 @@
         </v-slide-y-transition>
       </v-container>
     </main>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-    >
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed">
+    <v-footer fixed="true">
       <span>&copy; 2017</span>
     </v-footer>
   </v-app>
@@ -84,16 +52,12 @@
   export default {
     data () {
       return {
-        clipped: false,
         drawer: true,
-        fixed: false,
         items: [
           { icon: 'bubble_chart', title: 'Inspire' }
         ],
         miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        title: 'ZenUml - A Text to Sequence Diagram Convertor'
       }
     }
   }
